4e7e7cb214a8593dce7ebdc0d14edb63e7e65830
Gabriel <gabrielcssg@gmail.com>
2019-10-17 17:50:52 -0300

    adição de login/logout, adicionado tela inicial

--- combat_aa/urls.py
+++ combat_aa/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('paciente/', include('paciente.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
